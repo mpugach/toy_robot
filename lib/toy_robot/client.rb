6ff1798152c6faa5ac68f49fe01fe1af73fe2d0a
Maksym Pugach <pugach.m@gmail.com>
2017-07-23 16:05:25 +0300

    implement  client

--- lib/toy_robot/client.rb
+++ lib/toy_robot/client.rb
@@ -1,7 +1,15 @@
 module ToyRobot
   class Client
     def self.start
-      puts 'ololo'
+      robot = ToyRobot::Robot.new
+
+      while (input = gets)
+        command, *arguments = input.chomp.split(/[\s,]+/).map(&:downcase)
+
+        next unless ToyRobot::Robot::COMMANDS.include?(command)
+
+        robot.public_send(command, *arguments)
+      end
     end
   end
 end
