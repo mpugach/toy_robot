ceff1b399d152d682a43a8d0a2554e63e9a68db9
Maksym Pugach <pugach.m@gmail.com>
2017-07-23 16:54:01 +0300

    ToyRobot::Robot#move tests

--- test/robot_test.rb
+++ test/robot_test.rb
@@ -49,4 +49,62 @@ describe ToyRobot::Robot do
       assert_silent { @robot.report }
     end
   end
+
+  describe "#move" do
+    def test_it_can_move_east
+      @robot.place("2", "2", "east")
+      @robot.move
+
+      assert_output("2,3,EAST\n") { @robot.report }
+    end
+
+    def test_it_can_move_west
+      @robot.place("2", "2", "west")
+      @robot.move
+
+      assert_output("2,1,WEST\n") { @robot.report }
+    end
+
+    def test_it_can_move_north
+      @robot.place("2", "2", "north")
+      @robot.move
+
+      assert_output("3,2,NORTH\n") { @robot.report }
+    end
+
+    def test_it_can_move_south
+      @robot.place("2", "2", "south")
+      @robot.move
+
+      assert_output("1,2,SOUTH\n") { @robot.report }
+    end
+
+    def test_it_does_not_fall_east
+      @robot.place("0", "4", "east")
+      @robot.move
+
+      assert_output("0,4,EAST\n") { @robot.report }
+    end
+
+    def test_it_does_not_fall_west
+      @robot.place("0", "0", "west")
+      @robot.move
+
+      assert_output("0,0,WEST\n") { @robot.report }
+    end
+
+    def test_it_does_not_fall_north
+      @robot.place("4", "0", "north")
+      @robot.move
+
+      assert_output("4,0,NORTH\n") { @robot.report }
+    end
+
+    def test_it_does_not_fall_south
+      @robot.place("0", "0", "south")
+      @robot.move
+
+      assert_output("0,0,SOUTH\n") { @robot.report }
+    end
+  end
 end
