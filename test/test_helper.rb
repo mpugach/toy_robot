14c4a1d0598e787cb3d0f63124afbd5b16640b07
Maksym Pugach <pugach.m@gmail.com>
2017-07-23 16:46:50 +0300

    ToyRobot::VERSION test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "toy_robot"
 
 require "minitest/autorun"
+require 'minitest/pride'
+require 'minitest/hell'
---
+++ test/version_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+describe ToyRobot::VERSION do
+  def test_that_it_has_a_version_number
+    refute_nil ::ToyRobot::VERSION
+  end
+end
